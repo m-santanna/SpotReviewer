8f7bda8d0fb9ad9a3b04348807ac3965872047cc
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2023-11-20 22:06:57 +0000

    electron app inicitated

---
+++ my-new-app/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/renderer/**/*.{js,ts,jsx,tsx,css}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
