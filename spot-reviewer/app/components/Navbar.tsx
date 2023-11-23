c17826450cd5930d7a9706af4c723caf05851486
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2023-11-23 21:34:26 +0000

    navbar and landing section updates

--- spot-reviewer/app/components/Navbar.tsx
+++ spot-reviewer/app/components/Navbar.tsx
@@ -1,12 +1,7 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
-import React from 'react'
 
 const NavItems = [
-  {
-    name: 'DOT',
-    path: '/',
-  },
   {
     name: 'Objectives',
     path: '/',
@@ -25,11 +20,16 @@ const Navbar = () => {
   return (
     <nav className="h-[70px] w-screen bg-slate-900 sticky z-10 top-0 border-b border-white flex flex-col justify-center px-4">
       <ul className="flex justify-between">
-        {NavItems.map((item) => (
-          <Link href={item.path} key={item.name}>
-            {item.name}
-          </Link>
-        ))}
+        <Link href="/" className="font-semibold text-2xl">
+          DOT
+        </Link>
+        <div>
+          {NavItems.map((item) => (
+            <Link href={item.path} key={item.name} className="px-4">
+              {item.name}
+            </Link>
+          ))}
+        </div>
 
         <SignedIn>
           <UserButton />
--- spot-reviewer/app/components/sections/LandingSection.tsx
+++ spot-reviewer/app/components/sections/LandingSection.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import AnimatableDiv from '../AnimatableDiv'
-
 const LandingSection = () => {
   return (
-    <div className="screen-height-minus-navbar flex justify-center items-center">
+    <div className="screen-height-minus-navbar flex justify-center items-center relative">
       <AnimatableDiv className="text-6xl text-amber-200 animate-fade-up">
         The future of skin cancer prevention
       </AnimatableDiv>
