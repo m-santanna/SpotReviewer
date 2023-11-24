bc6d3f099830fa6b06b6cf5371e3545040d95ec0
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2023-11-24 00:21:42 +0000

    basic update on the contact section

--- spot-reviewer/app/components/sections/ContactSection.tsx
+++ spot-reviewer/app/components/sections/ContactSection.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import AnimatableDiv from '../AnimatableDiv'
+import Link from 'next/link'
 
 const ContactSection = () => {
   return (
-    <div className="screen-height-minus-navbar flex justify-center items-center">
+    <div className="screen-height-minus-navbar flex flex-col">
       <AnimatableDiv
-        className="text-4xl font-bold animate-fade-up"
+        className="text-4xl font-bold animate-fade-up text-center mt-10"
         offset="-100px"
       >
-        CONTACT
+        Contact us!
+      </AnimatableDiv>
+      <AnimatableDiv
+        className="text-2xl animate-fade-up flex justify-center mt-20"
+        offset="-200px"
+      >
+        <div>
+          Email:{' '}
+          <Link href="mailto:matheus.santanna03@gmail.com">
+            matheus.santanna03@gmail.com
+          </Link>
+        </div>
       </AnimatableDiv>
     </div>
   )
