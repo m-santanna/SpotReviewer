dafe6df61e8c29aa31e8af6cf5121dd60fb2031f
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2024-01-22 19:04:01 +0000

    lets see

--- my-new-app/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/renderer/**/*.{js,ts,jsx,tsx,css}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
